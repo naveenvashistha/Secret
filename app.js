6df6d2168156d4d1d15e0db9aef15767e1696d1c
Naveen Vashistha <naveenvashistha2000@gmail.com>
2025-08-07 01:56:08 +0530

    changed public folder directory link for prod

--- app.js
+++ app.js
@@ -14,7 +14,7 @@ const path = require("path");
 const app = express();
 
 app.use(express.urlencoded());
-app.use(express.static("public"));
+app.use(express.static(path.join(__dirname, "public")));
 app.set('views', path.join(__dirname, 'views'));
 app.set("view engine","ejs");
 
